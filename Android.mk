8026d371bf649b6f1c09ca8e4ceb06602c7e6579
Evan Anderson <evananderson@aospa.co>
2016-10-27 00:03:22 -0400

    hals: restructure for CAF
    * msm8994 hal only
    
    Change-Id: I01fcfeae1310ec662ab355f2620ba6a76bd014a2
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- Android.mk
+++ Android.mk
@@ -1,27 +1,10 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
-    #This is for 8084 based platforms
-    include $(call all-named-subdir-makefiles,msm8084)
-  else
-    ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
-      include $(call all-named-subdir-makefiles,msm8226)
-    else
-      ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-        include $(call all-named-subdir-makefiles,msm8960)
-      else
-        ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-          include $(call all-named-subdir-makefiles,msm8994)
-        else
-          ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-            include $(call all-named-subdir-makefiles,msm8909)
-          else
-            ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-              include $(call all-named-subdir-makefiles,msm8996)
-            endif
-          endif
-        endif
-      endif
-    endif
-  endif
+display-hals := libgralloc libcopybit liblight libmemtrack libqservice
+display-hals += libhwcomposer liboverlay libqdutils libhdmi
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+else
+ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif

b77ff06280596d438954355bd26815bad018b0dc
Thierry Strudel <tstrudel@google.com>
2018-05-08 13:58:41 -0700

    don't build some components when vendor/ is missing
    Bug: 68817732
    Change-Id: If81e5e72cabf738854ceb52250f7891967a16f91
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- Android.mk
+++ Android.mk
@@ -9,11 +9,13 @@ endif
 
 display-hals += gralloc
 
+ifneq ($(BUILD_WITHOUT_VENDOR),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
 endif
 endif #TARGET_DISABLE_DISPLAY

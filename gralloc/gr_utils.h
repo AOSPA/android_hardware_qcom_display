c977cea8feef50579b79a774bb18a150ffc75dde
Arun Kumar K.R <akumarkr@codeaurora.org>
2018-06-15 16:31:23 +0530

    Gralloc: Do not use adreno for secure camera and secure display
    Avoid using adreno size calculations for secure camera and
    secure display buffers.
    
    Bug: 110055706
    Test: adb shell keystore_cli_v2 confirmation --prompt_text=testfdasdf --extra_data=aa --locale=en
    Change-Id: Ib61d893cbcc57b10070250a63b8fa79106de4af3
    Acked-by: Saurabh Dubey <sdubey@codeaurora.org>

--- gralloc/gr_utils.h
+++ gralloc/gr_utils.h
@@ -106,6 +106,7 @@ uint32_t GetDataAlignment(int format, uint64_t usage);
 void GetGpuResourceSizeAndDimensions(const BufferInfo &info, unsigned int *size,
                                      unsigned int *alignedw, unsigned int *alignedh,
                                      GraphicsMetadata *graphics_metadata);
+bool CanUseAdrenoForSize(int buffer_type, uint64_t usage);
 bool GetAdrenoSizeAPIStatus();
 }  // namespace gralloc
 

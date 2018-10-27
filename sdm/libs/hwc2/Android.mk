02abe83a9ecf8b3de6f6feeb3aeb95b3b4a3c2c2
Chih-Hung Hsieh <chh@google.com>
2018-10-27 01:35:00 -0700

    Allow implicit-fallthrough warnings in hwc_display.cpp.
    It will be a global error by default.
    
    Test: make checkbuild
    Bug: 112564944
    Change-Id: I1b6724606d756f3584dd9e482881f33eada64177
    Exempt-From-Owner-Approval: do not block on new warnings

--- sdm/libs/hwc2/Android.mk
+++ sdm/libs/hwc2/Android.mk
@@ -45,6 +45,9 @@ LOCAL_SHARED_LIBRARIES        += vendor.display.config@1.3
 LOCAL_SHARED_LIBRARIES        += vendor.display.config@1.4
 endif
 
+# Allow implicit fallthroughs in hwc_display.cpp until they are fixed.
+LOCAL_CFLAGS                  += -Wno-error=implicit-fallthrough
+
 LOCAL_SRC_FILES               := hwc_session.cpp \
                                  hwc_session_services.cpp \
                                  hwc_display.cpp \

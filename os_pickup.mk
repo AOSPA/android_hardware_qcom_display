d259dae2f489a0a5c5bf2dd36e9bdeebaabddbc1
Cyan_Hsieh <cyanhsieh@google.com>
2019-01-22 19:27:41 +0800

    switch sm8150 to msmnile
    Bug: 123059966
    Change-Id: I665a7cf09b2cdff433523b590203bb2b387a9c0e

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_BOARD_PLATFORM),sm8150)
+ifeq ($(TARGET_BOARD_PLATFORM),msmnile)
 LOCAL_PATH := $(call my-dir)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))

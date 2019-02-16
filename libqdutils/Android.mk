adc30f7caf6fd07c8c458355c110791ef9a25020
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-02-16 21:34:05 +0530

    display: Partially convert libqdutils makefile to blueprint
     * Required by CAF surfaceflinger

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/../common.mk
-include $(CLEAR_VARS)
 
+include $(LOCAL_PATH)/../common.mk
+
+include $(CLEAR_VARS)
 LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
@@ -16,34 +17,4 @@ LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  comptype.cpp qd_utils.cpp \
                                  cb_utils.cpp  display_config.cpp \
                                  cb_swap_rect.cpp
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
-LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
-LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
-LOCAL_CFLAGS                    := $(common_flags)
-LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
-LOCAL_MODULE_TAGS               := optional
-LOCAL_MODULE                    := libqdMetaData
-LOCAL_VENDOR_MODULE             := true
-include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
-LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
-LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
-LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
-LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
-
-LOCAL_MODULE_TAGS               := optional
-LOCAL_MODULE                    := libqdMetaData.system
-
 include $(BUILD_SHARED_LIBRARY)

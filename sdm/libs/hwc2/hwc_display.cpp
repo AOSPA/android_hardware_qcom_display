03b7d913c9c4634ff573db0923624880d4d5868c
Sushil Chauhan <sushilchauhan@codeaurora.org>
2019-06-21 02:06:24 -0700

    sdm: Allow Skip Validate after Idle Power Collapse
    After receiving Idle Power collapse event in HWC, Skip Validate was
    disabled with a cost of 2ms in ValidateDisplayInternal in next frame.
    Skip Validate can be allowed with current Idle Power collapse policy.
    
    CRs-Fixed: 2478233
    Change-Id: I5e23c8a97bc2e7205c8e62886e870c1d5936abf6

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -1216,8 +1216,7 @@ DisplayError HWCDisplay::HandleEvent(DisplayEvent event) {
       validated_ = false;
       break;
     }
-    case kThermalEvent:
-    case kIdlePowerCollapse: {
+    case kThermalEvent: {
       SEQUENCE_WAIT_SCOPE_LOCK(HWCSession::locker_[id_]);
       validated_ = false;
     } break;
@@ -1231,6 +1230,8 @@ DisplayError HWCDisplay::HandleEvent(DisplayEvent event) {
               id_);
       }
     } break;
+    case kIdlePowerCollapse:
+      break;
     default:
       DLOGW("Unknown event: %d", event);
       break;

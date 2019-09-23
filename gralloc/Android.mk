073430b487a8a95949e12a433d8bddd37c5f7168
LuK1337 <priv.luk@gmail.com>
2019-09-23 21:41:37 +0200

    gralloc: Allow devices to opt-in for YCrCb camera preview
    * Some stock camera HAL implementations output video
      stream in YCrCb instead of YCbCr.
    
    Change-Id: I562847dfd9bcf796052f6fd7263e2affbd601384

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -42,6 +42,9 @@ LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-si
 ifeq ($(TARGET_NEEDS_RAW10_BUFFER_FIX),true)
 LOCAL_CFLAGS                  += -DRAW10_BUFFER_FIX
 endif
+ifeq ($(TARGET_USES_YCRCB_CAMERA_PREVIEW),true)
+LOCAL_CFLAGS                  += -DUSE_YCRCB_CAMERA_PREVIEW
+endif
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
 include $(BUILD_SHARED_LIBRARY)

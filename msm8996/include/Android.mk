41886fca6995b6e381527cea225aed524bcbae0b
Jake Weinstein <jake@aospa.co>
2020-03-28 23:07:25 +0100

    msm8996: sync includes with quartz-89xx
    Change-Id: I2332a36bf27e5f01d341272d1c5d5df27181d103

--- msm8996/include/Android.mk
+++ msm8996/include/Android.mk
@@ -7,7 +7,9 @@ include $(CLEAR_VARS)
 # LOCAL_HEADER_LIBRARIES := display_headers
 LOCAL_VENDOR_MODULE           := true
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := ../libqdutils/qd_utils.h \
+LOCAL_COPY_HEADERS            := color_metadata.h \
+                                 display_properties.h \
+                                 ../libqdutils/qd_utils.h \
                                  ../libqdutils/qdMetaData.h \
                                  ../libqdutils/display_config.h \
                                  ../libqservice/QServiceUtils.h \

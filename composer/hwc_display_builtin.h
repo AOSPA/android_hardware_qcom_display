921e0a353b2e11f3d4f13d91800b7c16e50db09a
junj <junj@codeaurora.org>
2020-10-24 16:01:46 +0800

    Revert "composer: update the active mode index post-commit"
    This reverts commit 1f3fb8d9b3b6d386f8b164c15e88ef0771401f44.
    
    Change-Id: I7ddd33ef2c5102731efefcdcd8093372df15471e

--- composer/hwc_display_builtin.h
+++ composer/hwc_display_builtin.h
@@ -224,7 +224,6 @@ class HWCDisplayBuiltIn : public HWCDisplay, public SyncTask<LayerStitchTaskCode
   bool is_smart_panel_ = false;
   const char *kDisplayBwName = "display_bw";
   bool enable_bw_limits_ = false;
-  bool disable_dyn_fps_ = false;
 };
 
 }  // namespace sdm

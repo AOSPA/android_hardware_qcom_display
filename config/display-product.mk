7c773d5c3de44dbd4d4c21bb3005821f76fe20c5
Marco Marinho <marcomarinho12@gmail.com>
2020-11-14 20:46:14 +0000

    display: Add config makefiles
    Change-Id: I4e6c614a260d3135593259451a4262c0597fa3cb

---
+++ config/display-product.mk
@@ -0,0 +1,22 @@
+# Display product definitions
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.$(TARGET_BOARD_PLATFORM) \
+    hwcomposer.$(TARGET_BOARD_PLATFORM) \
+    memtrack.$(TARGET_BOARD_PLATFORM) \
+    libsdmcore \
+    libsdmutils \
+    libqdMetaData \
+    libdisplayconfig.vendor \
+    libtinyxml \
+    libvulkan
+
+#Set WCG properties
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_virtual_display_dimension=4096

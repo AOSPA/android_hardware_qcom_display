a13c07ab750ec82f014a6f49ef05f4a4bcb19822
Marco Marinho <marcomarinho12@gmail.com>
2020-11-14 20:46:14 +0000

    display: Add config makefiles
    * from LA.UM.9.2.r1-01000-SDMxx0.0
    * converted some board configs into system props
    
    Change-Id: I4e6c614a260d3135593259451a4262c0597fa3cb

---
+++ config/display-product.mk
@@ -0,0 +1,38 @@
+# Display product definitions
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.$(TARGET_BOARD_PLATFORM) \
+    hwcomposer.$(TARGET_BOARD_PLATFORM) \
+    memtrack.$(TARGET_BOARD_PLATFORM) \
+    libsdmcore \
+    libsdmutils \
+    libqdMetaData \
+    libdisplayconfig.vendor \
+    libtinyxml \
+    libvulkan \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.demo.hdmirotationlock=false \
+    debug.sf.enable_hwc_vds=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
+    debug.mdpcomp.logs=0 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.hwc.mdpcomp.enable=true \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1
+
+#Set WCG properties
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_virtual_display_dimension=4096

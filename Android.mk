2ee5c653e461dfab618266ef8c7775138b5a1701
Zube Molokwu <zmolokwu@codeaurora.org>
2021-01-26 21:20:50 -0800

    display: Remove libmemtrack library
    Change-Id: I020739e3bd7ff3044a34f75c5bfe8a0f97455f8e

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit libmemtrack hdmi_cec \
+    display-hals += libcopybit hdmi_cec \
                     $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils
 endif
 

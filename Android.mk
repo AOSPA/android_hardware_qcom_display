01d4ede0c101765d228c8a7ab557a60c4f09a90f
Zube Molokwu <zmolokwu@codeaurora.org>
2021-01-26 21:20:50 -0800

    display: Remove libmemtrack library
    Change-Id: I020739e3bd7ff3044a34f75c5bfe8a0f97455f8e

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug gpu_tonemapper
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit liblight hdmi_cec \
                     libdrmutils libhistogram drm.vendor
 endif
 
--- config/display-product.mk
+++ config/display-product.mk
@@ -8,12 +8,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.$(TARGET_BOARD_PLATFORM) \
     lights.$(TARGET_BOARD_PLATFORM) \
     hwcomposer.$(TARGET_BOARD_PLATFORM) \
-    memtrack.$(TARGET_BOARD_PLATFORM) \
     libsdmcore \
     libsdmutils \
     libqdMetaData \

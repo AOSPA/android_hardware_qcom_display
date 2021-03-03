7c48c8d2cbdfdc2d653ff2298ae0212f6d150e6f
williamtai <williamtai@google.com>
2021-03-03 12:09:27 +0800

    hwc: Disable HDR10+ support in HWC
    Bug: 175239925
    Test: adb shell dumpsys SurfaceFlinger |grep hdr
    HWC Support: wideColorGamut=true hdr10plus=false hdr10=true
    
    Change-Id: I7acfdb0f1d6020475cdec31382404ae33628611e

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -1718,13 +1718,12 @@ HWC2::Error HWCDisplay::GetHdrCapabilities(uint32_t *out_num_types, int32_t *out
   }
 
   if (out_types == nullptr) {
-    // We support HDR10, HLG and HDR10_PLUS.
-    *out_num_types = 3;
+    // We support HDR10 and HLG.
+    *out_num_types = 2;
   } else {
-    // HDR10, HLG and HDR10_PLUS are supported.
+    // HDR10 and HLG are supported.
     out_types[0] = HAL_HDR_HDR10;
     out_types[1] = HAL_HDR_HLG;
-    out_types[2] = HAL_HDR_HDR10_PLUS;
     *out_max_luminance = fixed_info.max_luminance;
     *out_max_average_luminance = fixed_info.average_luminance;
     *out_min_luminance = fixed_info.min_luminance;

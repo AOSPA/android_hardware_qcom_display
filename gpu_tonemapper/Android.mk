9caa9ad0db770f85335f347c558696dc11f74af6
Manjunatha Madana <mamanj@codeaurora.org>
2021-11-09 16:17:15 +0530

    display: Add kernel header dependency for vendor builds
    Change-Id: Ibdcf74cac01ef2fd7ca1da5f82f10676dbf56800

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -16,6 +16,7 @@ LOCAL_C_INCLUDES          := $(TARGET_OUT_HEADERS)/qcom/display/
 LOCAL_C_INCLUDES          += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SHARED_LIBRARIES    := libEGL libGLESv2 libGLESv3 libui libutils liblog
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_CFLAGS              := $(version_flag) -Wno-missing-field-initializers -Wall \
                              -Wno-unused-parameter -Wno-unreachable-code-loop-increment -DLOG_TAG=\"GPU_TONEMAPPER\"

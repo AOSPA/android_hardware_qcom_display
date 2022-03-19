dcfd8dd8aff7b33474b96663a1dc20f0269ee8de
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-19 14:08:26 +0900

    config: product: bring inline with lahaina/qssi interfaces
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: If09d2bfebd969fc438f0435e965c71285a21ad02

--- config/display-product.mk
+++ config/display-product.mk
@@ -18,6 +18,9 @@ PRODUCT_PACKAGES += \
     libsdmutils \
     libqdMetaData \
     libdisplayconfig.vendor \
+    libdisplayconfig.qti.vendor \
+    vendor.display.config@1.16.vendor \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \

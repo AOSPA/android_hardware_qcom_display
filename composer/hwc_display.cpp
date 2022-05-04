31211d57ca7f67ccc662c3ce511cf40733da401c
Chenyang Zhong <zhongcy95@gmail.com>
2022-05-04 08:39:57 -0400

    hwc_display: Guard Dolby Vision support
    To enable Dolby Vision support in hwc, add the following to your
    BoardConfig.mk or BoardConfigCommon.mk
    
    SOONG_CONFIG_NAMESPACES += dolby_vision
    SOONG_CONFIG_dolby_vision += enabled
    SOONG_CONFIG_dolby_vision_enabled := true
    
    Change-Id: Ic512456847d84a1f5af64ed9fb8c38d7de4e8970

--- composer/hwc_display.cpp
+++ composer/hwc_display.cpp
@@ -1734,18 +1734,33 @@ HWC2::Error HWCDisplay::GetHdrCapabilities(uint32_t *out_num_types, int32_t *out
 
   uint32_t num_types = 0;
   if (fixed_info.hdr_plus_supported) {
+#ifdef TARGET_SUPPORTS_DOLBY_VISION
     num_types = UINT32(Hdr::HDR10_PLUS);
   } else {
     num_types = UINT32(Hdr::HLG);
   }
 
   // We support DOLBY_VISION, HDR10, HLG and HDR10_PLUS.
+#else
+    num_types = UINT32(Hdr::HDR10_PLUS) - 1;
+  } else {
+    num_types = UINT32(Hdr::HLG) - 1;
+  }
+
+  // We support HDR10, HLG and HDR10_PLUS.
+#endif /* TARGET_SUPPORTS_DOLBY_VISION */
   if (out_types == nullptr) {
     *out_num_types = num_types;
   } else {
     uint32_t max_out_types = std::min(*out_num_types, num_types);
     int32_t type = static_cast<int32_t>(Hdr::DOLBY_VISION);
     for (int32_t i = 0; i < max_out_types; i++) {
+#ifndef TARGET_SUPPORTS_DOLBY_VISION
+      while (type == static_cast<int32_t>(Hdr::DOLBY_VISION) /* Skip list */) {
+        // Skip the type
+        type++;
+      }
+#endif /* TARGET_SUPPORTS_DOLBY_VISION */
       if (type > (num_types + 1)) {
         break;
       }

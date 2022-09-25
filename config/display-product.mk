d8b01b7f02a5d78cb6281e6d847a8f025c944962
Jake Weinstein <jake@aospa.co>
2022-09-25 10:39:11 +0900

    config: Use TARGET_USES_FOD_ZPOS to enable UDFPS ZPOS
    Lineage OS duplicates the QCOM Display soong
    conditionals to their BoardConfig at
    https://github.com/LineageOS/android_vendor_lineage/commit/902f4474ba5759eb0711f30807723f82672fae6e since they do
    not all inherit the display makefiles from Qualcomm.
    I prefer not to duplicate the logic and would
    rather follow what Qualcomm is doing, which
    is using normal conditionals from device tree
    to define soong conditionals in the display HAL.
    
    Change-Id: I3d41c2748c1c8d32251d0b82f5da05c15d06c791

--- config/display-product.mk
+++ config/display-product.mk
@@ -191,6 +191,10 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplay_displayconfig_enabled := true
 endif
 
+ifeq ($(TARGET_USES_FOD_ZPOS), true)
+    SOONG_CONFIG_qtidisplay_udfps_enabled := true
+endif
+
 ifeq ($(TARGET_IS_HEADLESS), true)
     PRODUCT_SOONG_NAMESPACES += hardware/qcom/display/qmaa
     SOONG_CONFIG_qtidisplay_headless := true

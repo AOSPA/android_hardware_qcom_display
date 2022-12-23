6425851c21213c14c202d9ac1e8dbb3c8af90eb5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-23 06:01:35 +0530

    display: Make reserved_size in gralloc handle optional
    Few devices that use older stock blobs don't support this yet.
    Allow them to disable it via setting
    TARGET_GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE := true
    in BoardConfig, otherwise enabled by default.
    
    Change-Id: Icf12b8583ceec73d085557e2755e3799308e6c6a

--- config/display-product.mk
+++ config/display-product.mk
@@ -142,6 +142,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_hw_recovery_dump=1
 endif
 
+# Soong Namespace
+SOONG_CONFIG_NAMESPACES += qtidisplay
+
+# Soong Keys
+SOONG_CONFIG_qtidisplay := gralloc_handle_has_no_reserved_size
+
+# Soong Values
+SOONG_CONFIG_qtidisplay_gralloc_handle_has_no_reserved_size := false
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_RESERVED_SIZE),true)
+    SOONG_CONFIG_qtidisplay_gralloc_handle_has_no_reserved_size := true
+endif
+
 # Properties using default value:
 #    vendor.display.disable_hw_recovery=0
 

d334b2583a5774ccc3e2c394efc98c3026040e2e
Alex Sarraf <quic_asarraf@quicinc.com>
2023-01-27 08:35:12 -0800

    config: Set debug.sf.auto_latch_unsignaled to 0.
    Need to explicitly set auto_latch_unsignaled property to
    disabled state to keep latch-unsignaled as the default
    behavior.
    
    Change-Id: I3856caaf0a68e7e720819f25338d1eb9196f56b0

--- config/display-product.mk
+++ config/display-product.mk
@@ -86,6 +86,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
+    debug.sf.auto_latch_unsignaled=0 \
     debug.mdpcomp.logs=0 \
     vendor.gralloc.disable_ubwc=0 \
     vendor.display.disable_scaler=0 \

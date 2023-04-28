51f0b728972f91588ba6ff9af5a3bda23eb15810
Mashopy <eliasgheeraert@gmail.com>
2023-04-28 19:56:02 +0200

    display: Conditionally exclude libmemtrack
    Change-Id: Idf3df8f605425920179282c367d089b02f3cfe7e

--- Android.mk
+++ Android.mk
@@ -3,11 +3,15 @@ sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit libmemtrack hdmi_cec \
+    display-hals += libcopybit hdmi_cec \
                     $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils
 endif
 
 display-hals += gralloc
+
+ifneq ($(TARGET_DISABLE_MEMTRACK), true)
+    display-hals += libmemtrack
+endif
 
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
     display-hals += liblight

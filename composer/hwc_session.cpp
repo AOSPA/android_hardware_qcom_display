a4acf769506c4fc04f0755569911c10f3f566bfa
chandu078 <chandudyavanapelli03@gmail.com>
2023-06-21 09:43:18 +0200

    display: fix remaining code issues
    Change-Id: Ie1e17eb9d8a2e39790cfb39d6569ca7424c681c6
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -3545,7 +3545,7 @@ void HWCSession::HandleSecureSession() {
     }
   }
   if (is_active_virtual_display) {
-    auto error = DestroyVirtualDisplay(client_id);
+    [[maybe_unused]] auto error = DestroyVirtualDisplay(client_id);
   }
 
   // If it is called during primary prepare/commit, we need to pause any ongoing commit on
@@ -4088,7 +4088,7 @@ HWC3::Error HWCSession::WaitForResources(bool wait_for_resources, Display active
             cached_retire_fence_) {
           Fence::Wait(cached_retire_fence_);
         }
-        cached_retire_fence_ == nullptr;
+        cached_retire_fence_ = nullptr;
       }
       {
         SCOPE_LOCK(locker_[display_id]);

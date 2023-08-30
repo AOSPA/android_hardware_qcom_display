0a4255ec8ce05daac4f2720e7d6217433b7c0a48
Swastik Ingle <quic_single@quicinc.com>
2023-08-30 18:07:17 -0400

    config: Add missing layer buffer slots cache clear property
    Add missing layer buffer slots cache clear property used to enable
    IComposer3Client setLayerCommands API to explicitly clear buffer
    cache slots for a layer.
    
    Change-Id: I26a5e3e0cce05bd05baaadfaf0d8f4e68f9fa8e4
    CRs-Fixed: 3586598
    (cherry picked from commit 0c292ba6d8c33eaf966dd4428ca3d8e5292b35dd)

--- config/display-product.mk
+++ config/display-product.mk
@@ -162,6 +162,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_virtual_display_dimension=4096
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.clear_slots_with_set_layer_buffer=false
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 # Recovery is enabled, logging is enabled

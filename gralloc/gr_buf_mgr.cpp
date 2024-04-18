bd811cd38679a53d42a668d33e765954319fc546
Qing Huang <quic_huangq@quicinc.com>
2024-04-18 11:24:20 +0900

    gralloc: support PQ transfer in dataspace
    Google start to query color dataspace from gralloc if not
    produced by APP in screen capture. In HDR playback case,
    gralloc return unsupport for PQ transfer.
    Change adds that return correct value for PQ transfer.
    
    Change-Id: I15b8d720e5685ad53855f27f356cafddeb12f927
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- gralloc/gr_buf_mgr.cpp
+++ gralloc/gr_buf_mgr.cpp
@@ -189,7 +189,6 @@ static Error dataspaceToColorMetadata(Dataspace dataspace, ColorMetaData *color_
       Transfer_sYCC
       Transfer_BT2020_2_1
       Transfer_BT2020_2_2
-      Transfer_SMPTE_ST2084
       Transfer_ST_428
       */
   }
@@ -269,6 +268,9 @@ static Error colorMetadataToDataspace(ColorMetaData color_metadata, Dataspace *d
       break;
     case Transfer_HLG:
       transfer = Dataspace::TRANSFER_HLG;
+      break;
+    case Transfer_SMPTE_ST2084:
+      transfer = Dataspace::TRANSFER_ST2084;
       break;
     default:
       return Error::UNSUPPORTED;

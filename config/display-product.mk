b37f182deb7171cef9e0ffb460f5b191bf0ba15b
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-09-02 17:31:32 +0000

    display: Kill legacy lights service
    * Considering https://gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/41124
    
    Change-Id: I230d6d3d40e265c72e5a1cc9d98068e1a7da47a2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- config/display-product.mk
+++ config/display-product.mk
@@ -9,7 +9,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     gralloc.$(TARGET_BOARD_PLATFORM) \
-    lights.$(TARGET_BOARD_PLATFORM) \
     hwcomposer.$(TARGET_BOARD_PLATFORM) \
     libsdmcore \
     libsdmutils \

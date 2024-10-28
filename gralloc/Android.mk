57fad7319fb7fb50880cfdca4e4069bd764c7058
Jprimero15 <jprimero15@aospa.co>
2024-10-28 21:03:46 +0800

    Convert libgrallocutils to blueprint.
    * Satisfy the ELF check
    
    Change-Id: I877e966711eab91df4145b2b7d55dbcce107f8e0
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -24,31 +24,6 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_device_impl.cpp
 include $(BUILD_SHARED_LIBRARY)
 
-#libgrallocutils
-include $(CLEAR_VARS)
-LOCAL_MODULE                  := libgrallocutils
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl  \
-                                  android.hardware.graphics.common@1.2 \
-                                  android.hardware.graphics.mapper@2.0 \
-                                  android.hardware.graphics.mapper@2.1 \
-                                  android.hardware.graphics.mapper@3.0 \
-                                  android.hardware.graphics.mapper@4.0
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion \
-                                 -D__QTI_DISPLAY_GRALLOC__
-ifeq ($(TARGET_NEEDS_RAW10_BUFFER_FIX),true)
-LOCAL_CFLAGS                  += -DRAW10_BUFFER_FIX
-endif
-ifeq ($(TARGET_USES_YCRCB_CAMERA_PREVIEW),true)
-LOCAL_CFLAGS                  += -DUSE_YCRCB_CAMERA_PREVIEW
-endif
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 #libgralloccore
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgralloccore

88e54481ba9df4a65f330542171d63070de02f93
Jprimero15 <jprimero15@aospa.co>
2024-10-28 21:03:46 +0800

    dispaly: gralloc: Convert libgrallocutils to blueprint
    * Satisfy the ELF check
    
    Change-Id: I07dfd8e3e13e0bb858973917fb4b0745068d8888
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>
    
    [chrisl7]: Adapt to bengal/kona/lito 4.19
    
    Change-Id: I3c12316c64e09dc706fbb311d85520c74ea25e58
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -34,34 +34,6 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_device_impl.cpp
 include $(BUILD_SHARED_LIBRARY)
 
-#libgrallocutils
-include $(CLEAR_VARS)
-LOCAL_MODULE                  := libgrallocutils
-LOCAL_VENDOR_MODULE           := true
-LOCAL_SANITIZE                := integer_overflow
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl  \
-                                  android.hardware.graphics.common@1.2 \
-                                  android.hardware.graphics.mapper@2.0 \
-                                  android.hardware.graphics.mapper@2.1 \
-                                  android.hardware.graphics.mapper@3.0 \
-                                  android.hardware.graphics.mapper@4.0
-LOCAL_CFLAGS                  := $(common_flags) $(qmaa_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion \
-                                 -D__QTI_DISPLAY_GRALLOC__
-
-ifeq ($(TARGET_NO_RAW10_CUSTOM_FORMAT),true)
-    LOCAL_CFLAGS              += -DNO_RAW10_CUSTOM_FORMAT
-endif
-ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
-    LOCAL_CFLAGS              += -DUSE_YCRCB_CAMERA_ENCODE
-endif
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp gr_camera_info.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 #libgralloccore
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgralloccore

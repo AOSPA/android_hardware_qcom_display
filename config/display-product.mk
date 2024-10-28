3603b20ff1c27ae12a22574c43ac9148a4267275
Jprimero15 <jprimero15@aospa.co>
2024-10-28 21:03:46 +0800

    Convert libgrallocutils to blueprint.
    * Satisfy the ELF check
    
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>
    
    fixup! gralloc: Allow devices to opt-in for YCrCb camera video encode
    
    Add config/display-product.mk while at it.
    
    Change-Id: I877e966711eab91df4145b2b7d55dbcce107f8e0

--- config/display-product.mk
+++ config/display-product.mk
@@ -163,3 +163,16 @@ PRODUCT_PACKAGES += vendor.display.color@1.0.vendor \
                     vendor.display.color@1.0-service.rc \
                     ppd
 endif
+
+ifeq ($(TARGET_NEEDS_RAW10_BUFFER_FIX),true)
+$(call soong_config_set,gralloc,needs_raw10_buffer_fix,true)
+endif
+
+ifeq ($(TARGET_USES_YCRCB_CAMERA_PREVIEW),true)
+$(call soong_config_set,gralloc,uses_ycrcb_camera_preview,true)
+endif
+
+ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
+$(call soong_config_set,gralloc,uses_ycrcb_camera_encode,true)
+endif
+

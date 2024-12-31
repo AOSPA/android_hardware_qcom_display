9cfda715a4ba682c24ccc4e2f7542e9785244969
Michael Bestas <mkbestas@lineageos.org>
2024-12-31 20:11:04 +0200

    Convert remaining Android.mk to blueprint
    Change-Id: I2bc23d2111417355f95c4a2d6e42d2ec2003ead4

--- config/trinket.mk
+++ config/trinket.mk
@@ -7,7 +7,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
-    gralloc.trinket \
+    gralloc.qcom \
     hwcomposer.qcom \
     libqdutils \
     libqdMetaData \

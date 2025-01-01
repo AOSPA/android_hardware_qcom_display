c4a68e844449f8940525804ec2ef2657d834c501
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 02:03:40 +0200

    display: Remove deprecated lights.$(TARGET_BOARD_PLATFORM)
    AIDL light HAL is required as of U QPR3.
    
    Change-Id: I010fe28125c9532bfa02e5a6e3009f1fd7358d3f

--- config/talos.mk
+++ config/talos.mk
@@ -8,7 +8,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     gralloc.sm6150 \
-    lights.sm6150 \
     hwcomposer.sm6150 \
     libqdutils \
     libqdMetaData \

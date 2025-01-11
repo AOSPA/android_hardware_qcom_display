727fd0b40800b8807591c65cc5151921040dfca8
Jprimero15 <jprimero15@aospa.co>
2025-01-11 23:29:18 +0800

    Convert gralloc.89xx to blueprint
    Change-Id: I37d725bf4cc3f9913d3285ca711d655936d12e2b
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -4,26 +4,6 @@ include $(LOCAL_PATH)/../common.mk
 include $(LIBION_HEADER_PATH_WRAPPER)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_RELATIVE_PATH    := hw
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync libgrallocutils \
-                                 libgralloccore \
-                                 android.hardware.graphics.mapper@2.0 \
-                                 android.hardware.graphics.mapper@2.1 \
-                                 android.hardware.graphics.mapper@3.0 \
-                                 android.hardware.graphics.mapper@4.0
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wall -Werror \
-                                 -D__QTI_DISPLAY_GRALLOC__
-LOCAL_CLANG                   := true
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_SRC_FILES               := gr_device_impl.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 #libgralloccore
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgralloccore

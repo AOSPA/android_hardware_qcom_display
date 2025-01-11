99439077f997543ecbf141c91e0a2997b7e2296c
Jprimero15 <jprimero15@aospa.co>
2025-01-11 22:43:38 +0800

    Convert hwcomposer to blueprint
    Change-Id: I03532378e030359a1135b456bf85abaca59d50f4
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- config/display-product.mk
+++ config/display-product.mk
@@ -5,3 +5,15 @@ endif
 ifeq ($(TARGET_USES_DRM_PP),true)
 $(call soong_config_set,sdmcore,uses_drm_pp,true)
 endif
+
+ifeq ($(TARGET_EXCLUDES_DISPLAY_PP),true)
+$(call soong_config_set,hwc2,excludes_display_pp,true)
+endif
+
+ifeq ($(TARGET_EXCLUDES_MULTI_DISPLAY),true)
+$(call soong_config_set,hwc2,excludes_multi_display,true)
+endif
+
+ifeq ($(TARGET_HAS_WIDE_COLOR_DISPLAY),true)
+$(call soong_config_set,hwc2,has_wide_display,true)
+endif

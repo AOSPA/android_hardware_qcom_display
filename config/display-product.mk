f77fd4033c8151426439ac6a95c76e4aa87f12fa
Jprimero15 <jprimero15@aospa.co>
2025-01-11 21:37:34 +0800

    Convert libsdmcore to blueprint
    Change-Id: I184ba8870e9f92b773cd39b7a82db0bf349ad158
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- config/display-product.mk
+++ config/display-product.mk
@@ -1,3 +1,7 @@
 ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
 $(call soong_config_set,gralloc,uses_ycrcb_camera_encode,true)
 endif
+
+ifeq ($(TARGET_USES_DRM_PP),true)
+$(call soong_config_set,sdmcore,uses_drm_pp,true)
+endif

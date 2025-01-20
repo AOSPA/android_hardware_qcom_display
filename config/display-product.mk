68721775cf898a911d8acbd3cf40df708b505bff
Jprimero15 <jprimero15@aospa.co>
2025-01-21 07:51:16 +0800

    display: Streamline soong configs
    * Some bits of this is based on caf commit "display: Switch to soong".
    * Reorganize all soong configs placement while at it
    * Add missing TARGET_IS_HEADLESS
    * Reformat some blueprints a bit
    * Some cleanups
    
    Change-Id: I856de0c54fecf5a3b065d0c6990c6b0a0a6aef8e
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- config/display-product.mk
+++ config/display-product.mk
@@ -1,27 +1,31 @@
-ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
-$(call soong_config_set,gralloc,uses_ycrcb_camera_encode,true)
-endif
-
 ifeq ($(ENABLE_HYP),true)
-$(call soong_config_set,gralloc,enable_hyp,true)
-endif
-
-ifeq ($(TARGET_USES_GRALLOC4),true)
-$(call soong_config_set,gralloc,uses_gralloc4,true)
-endif
-
-ifeq ($(TARGET_USES_DRM_PP),true)
-$(call soong_config_set,sdmcore,uses_drm_pp,true)
+$(call soong_config_set,qtidisplay,enable_hyp,true)
 endif
 
 ifeq ($(TARGET_EXCLUDES_DISPLAY_PP),true)
-$(call soong_config_set,hwc2,excludes_display_pp,true)
+$(call soong_config_set,qtidisplay,excludes_display_pp,true)
 endif
 
 ifeq ($(TARGET_EXCLUDES_MULTI_DISPLAY),true)
-$(call soong_config_set,hwc2,excludes_multi_display,true)
+$(call soong_config_set,qtidisplay,excludes_multi_display,true)
 endif
 
 ifeq ($(TARGET_HAS_WIDE_COLOR_DISPLAY),true)
-$(call soong_config_set,hwc2,has_wide_display,true)
+$(call soong_config_set,qtidisplay,has_wide_display,true)
+endif
+
+ifeq ($(TARGET_IS_HEADLESS), true)
+$(call soong_config_set,qtidisplay,is_headless,true)
+endif
+
+ifeq ($(TARGET_USES_DRM_PP),true)
+$(call soong_config_set,qtidisplay,uses_drm_pp,true)
+endif
+
+ifeq ($(TARGET_USES_GRALLOC4),true)
+$(call soong_config_set,qtidisplay,uses_gralloc4,true)
+endif
+
+ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
+$(call soong_config_set,qtidisplay,uses_ycrcb_camera_encode,true)
 endif
